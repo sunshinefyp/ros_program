d032514b51c6bf00f0a42033052f207791c35793
sunshinefyp <1353381368@qq.com>
2020-03-23 22:16:18 +0800

    first ros commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ros/workspace/ros_program/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ros/workspace/ros_program/catkin_ws/devel;/home/ros/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ros/workspace/ros_program/catkin_ws/devel;/home/ros/catkin_ws/devel;/opt/ros/kinetic" != "" else []
